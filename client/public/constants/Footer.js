b758f073c959ed68fe4355d709b6a8be404acf10
yoantodorov <todorovyoan075@gmail.com>
2023-04-12 19:19:53 +0300

    Initial Commit - Implementation

---
+++ client/public/constants/Footer.js
@@ -0,0 +1,4 @@
+export const GITHUB_REPO_URL = "https://github.com/yoantodorovv/Portfolio";
+export const LINKED_IN_URL = "https://www.linkedin.com/in/yoan-todorov-22a261214/";
+export const GITHUB_URL = "https://github.com/yoantodorovv/";
+export const INSTAGRAM_URL = "https://www.instagram.com/yoantodorovv/";
---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import { Navigation } from './ components/Navigation/Navigation'
+import { Footer } from './ components/Footer/Footer'
+
+function App() {
+    return (
+        <div className="App">
+            <Navigation />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
