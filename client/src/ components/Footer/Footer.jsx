c211ab73ea1613be86ca798a907a50ed344e45f6
yoantodorov <todorovyoan075@gmail.com>
2023-04-12 19:20:09 +0300

    Added Navigation and Footer

---
+++ client/src/ components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import styles from './Footer.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons' 
+
+import * as constants from '../../../public/constants/Footer'
+
+export const Footer = () => {
+
+    const onLinkClick = (url) => {
+        console.log(url);
+
+        window.open(url, "_blank");
+    }
+
+    return (
+        <>
+            <div className={styles['socials']}>
+                <ul>
+                    <li
+                        onClick={() => onLinkClick(constants.LINKED_IN_URL)}
+                    >
+                        <FontAwesomeIcon icon={faLinkedinIn} size="xl" />
+                    </li>
+                    <li
+                        onClick={() => onLinkClick(constants.GITHUB_URL)}
+                    >
+                        <FontAwesomeIcon icon={faGithub} size="xl" />
+                    </li>
+                    <li
+                        onClick={() => onLinkClick(constants.INSTAGRAM_URL)}
+                    >
+                        <FontAwesomeIcon icon={faInstagram} size="xl" />
+                    </li>
+                </ul>
+            </div>
+            <div className={styles['wrapper']} onClick={() => onLinkClick(constants.GITHUB_REPO_URL)}>
+                <p>Designed & Built by Yoan Todorov</p>
+            </div>
+        </>
+    );
+}
---
+++ client/src/ components/Navigation/Navigation.jsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import styles from './Navigation.module.scss'
+
+export const Navigation = () => {
+    const initialState = {
+        about: false,
+        education: false,
+        techStack: false,
+        workflow: false,
+        contact: false,
+    };
+
+    const [isActive, setIsActive] = useState(initialState);
+
+    const handleNavClick = (propName) => setIsActive({...initialState, [propName]: true});
+
+    return (
+        <div className={styles['wrapper']}>
+            <div className={styles['logo']}>
+                <a href='#main'>Yoan Todorov</a>
+            </div>
+            <nav className={styles['nav']}>
+                <ul>
+                    <li>
+                        <a
+                            onClick={() => handleNavClick('about')}
+                            className={isActive.about ? styles['active'] : undefined}
+                            href='#about'>
+                                <span className={styles['highlight']}>
+                                    0.
+                                </span>
+                                About
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            onClick={() => handleNavClick('education')}
+                            className={isActive.education ? styles['active'] : undefined}
+                            href='#education'>
+                                <span className={styles['highlight']}>
+                                    1.
+                                </span>
+                                Education
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            onClick={() => handleNavClick('techStack')}
+                            className={isActive.techStack ? styles['active'] : undefined}
+                            href='#tech-stack'>
+                                <span className={styles['highlight']}>
+                                    2.
+                                </span>
+                                Tech Stack
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            onClick={() => handleNavClick('workflow')}
+                            className={isActive.workflow ? styles['active'] : undefined}
+                            href='#workflow'>
+                                <span className={styles['highlight']}>
+                                    3.
+                                </span>
+                                Workflow
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            onClick={() => handleNavClick('contact')}
+                            className={isActive.contact ? styles['active'] : undefined}
+                            href='#contact'>
+                                <span className={styles['highlight']}>
+                                    4.
+                                </span>
+                                Contact
+                        </a>
+                    </li>
+                    <li>
+                        <button
+                            type='button'
+                        >
+                            Resume
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
