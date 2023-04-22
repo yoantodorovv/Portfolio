3969e9a021df3229b782fc4dc71093d742a1306d
yoantodorov <todorovyoan075@gmail.com>
2023-04-22 12:40:16 +0300

    Updated tech stack card design by adding an icon

--- client/src/ components/TechStack/Card/Card.jsx
+++ client/src/ components/TechStack/Card/Card.jsx
@@ -4,23 +4,29 @@ import { db } from '../../../../services/firebase';
 import { getDoc, doc } from '@firebase/firestore';
 
 import styles from './Card.module.scss'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode, faServer, faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons'
+ 
 export const Card = ({
     field
 }) => {
     const [title, setTitle] = useState('');
+    const [icon, setIcon] = useState(null);
     const [dbField, setDbField] = useState({});
 
     useEffect(() => {
         switch (field) {
             case 'front-end':
                 setTitle('Front-End Stack');
+                setIcon(faCode);
                 break;
             case 'back-end':
                 setTitle('Back-End Stack');
+                setIcon(faServer);
                 break;
             default:
                 setTitle('Tools and Platforms');
+                setIcon(faScrewdriverWrench);
                 break;
         }
         
@@ -33,6 +39,7 @@ export const Card = ({
     return (
         <div className={styles['wrapper']}>
             <div className={styles['heading']}>
+                <FontAwesomeIcon className={styles['icon']} icon={icon} size="2x" />
                 <h2>{title}</h2>
             </div>
             <div className={styles['line']}></div>
