e8cb135a44444231fd5fc9b5555c6385f414a70e
yoantodorov <todorovyoan075@gmail.com>
2023-04-25 19:45:13 +0300

    fixed a bug

--- client/src/ components/Projects/Project/Project.jsx
+++ client/src/ components/Projects/Project/Project.jsx
@@ -18,7 +18,7 @@ export const Project = ({
                 <h2>{project.name}</h2>
                 <p>{project.year}</p>
                 <p>{project.description}</p>
-                <p className={styles['tech-stack']}>{project.techStack.map(x => <span key={x} className={styles['tech-stack-element']}>{x}</span>)}</p>
+                <div className={styles['tech-stack']}>{project.techStack.map(x => <p key={x} className={styles['tech-stack-element']}>{x}</p>)}</div>
                 <div className={styles['refer']}>
                     <a href={project.referLinks.github} target="_blank">
                         <FontAwesomeIcon icon={faGithub} size="xl" />
